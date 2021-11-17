32171198fdacc18b06fc3c610a3a4152bda58bdd
alailson <aalailson3@gmail.com>
2021-11-16 21:41:53 -0300

    fix logic

--- src/components/AccordionPanel/index.tsx
+++ src/components/AccordionPanel/index.tsx
@@ -4,8 +4,10 @@ import { ContextApi } from "../AccordionItem";
 import { Box } from "@chakra-ui/react";
 
 const AccordionPanel: FC = ({ children }) => {
-  const { isToggled } = useContext(ContextApi);
-  if (!isToggled) return <div></div>;
+  const { isToggled, itemId, currentActiveId } = useContext(ContextApi);
+
+  if (!isToggled || itemId !== currentActiveId) return <div></div>;
+
   return (
     <Box bg="#C94A4A" w="100%" p={4}>
       {children}
