89ad63c4b443fb9ea6846c37b7c39f55ca6d9cd6
alailson <aalailson3@gmail.com>
2021-11-16 23:52:06 -0300

    set store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import RoutesApp from "./routes";
+import { Provider } from 'react-redux'
 import './App.css'
+import configureStore from "./store/store";
+
+const { store } = configureStore()
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <RoutesApp />
-    </>
+    </Provider>
   );
 }
 
